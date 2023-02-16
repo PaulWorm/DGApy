c73a5e1211de33f12a9e40697c1d6a53f687236b
pworm <p.worm@a1.net>
2023-02-16 18:57:22 +0100

    Added automatic estimation for onset of asymptotic behavior.

--- src/SelfEnergy.py
+++ src/SelfEnergy.py
@@ -36,9 +36,9 @@ def fit_smom(iv=None, siwk=None, only_positive=True):
 class SelfEnergy():
     ''' class to handle self-energies'''
 
-    def __init__(self, sigma, beta, only_positive=True, smom0=None, smom1=None):
+    def __init__(self, sigma, beta, pos=True, smom0=None, smom1=None,err=5e-4):
         assert len(np.shape(sigma)) == 4, 'Currently only single-band SU(2) supported with [kx,ky,kz,v]'
-        if (not only_positive):
+        if (not pos):
             niv = sigma.shape[-1] // 2
             sigma = sigma[..., niv:]
 
@@ -46,6 +46,8 @@ class SelfEnergy():
         self.beta = beta
         iv_plus = mf.iv_plus(beta, self.niv)
         fit_mom0, fit_mom1 = fit_smom(iv_plus, sigma)
+
+        self.err = err
 
         # Set the moments for the symptotic behaviour:
         if (smom0 is None):
@@ -58,6 +60,9 @@ class SelfEnergy():
         else:
             self.smom1 = smom1
 
+        # estimate when the asymptotic behavior is sufficient:
+        self.niv_core = self.estimate_niv_core()
+
     @property
     def niv(self):
         return self.sigma.shape[-1]
@@ -65,6 +70,20 @@ class SelfEnergy():
     @property
     def nk(self):
         return self.sigma.shape[:-1]
+
+    def k_mean(self):
+        return np.mean(self.sigma, axis=(0, 1, 2))
+
+    def estimate_niv_core(self):
+        '''check when the real and the imaginary part are within error margin of the asymptotic'''
+        asympt = self.get_asympt(niv_asympt=self.niv)
+        ind_real = np.argmax(np.abs(self.k_mean().real-asympt.real) < self.err)
+        ind_imag = np.argmax(np.abs(self.k_mean().imag-asympt.imag) < self.err)
+        niv_core = max(ind_real,ind_imag)
+        if(niv_core < 20):
+            return 20
+        else:
+            return niv_core
 
     def get_siw(self, niv_core=None,niv_full = None):
         if(niv_core is None):
@@ -77,6 +96,13 @@ class SelfEnergy():
             sigma_asympt = np.concatenate((self.sigma[...,:niv_core], asympt), axis=-1)
             return np.concatenate((np.conj(np.flip(sigma_asympt, -1)), sigma_asympt), axis=-1)
 
+    def get_asympt(self,niv_asympt,pos=True):
+        iv_asympt = mf.iv_plus(self.beta, n=niv_asympt, n_min=0)
+        asympt = (self.smom0 - 1 / iv_asympt * self.smom1)
+        if(pos):
+            return asympt
+        else:
+            return np.concatenate((np.conj(np.flip(asympt, -1)), asympt), axis=-1)
 
 if __name__ == '__main__':
     import w2dyn_aux
@@ -89,16 +115,19 @@ if __name__ == '__main__':
     beta = dmft_file.get_beta()
     u = dmft_file.get_udd()
     n = dmft_file.get_totdens()
-    sigma_dmft = SelfEnergy(sigma=siw, beta=beta, only_positive=False)
+    sigma_dmft = SelfEnergy(sigma=siw, beta=beta, pos=False)
 
+    smom0 = get_smom0(u, n)
+    smom1 = get_smom1(u, n)
     print('---------------')
     print(f'Hartree: {get_smom0(u, n)}')
     print(f'Hartree fit: {sigma_dmft.smom0}')
     print(f'First moment: {get_smom1(u, n)}')
     print(f'First moment fit: {sigma_dmft.smom1}')
+    print(f'Niv core: {sigma_dmft.estimate_niv_core()}')
     print('---------------')
     niv_full  = 2000
-    niv_core = 200
+    niv_core = sigma_dmft.niv_core
     min_plot = niv_full+niv_core-50
     max_plot = niv_full+niv_core+50
     vn_asympt = mf.vn(niv_full)
@@ -107,4 +136,20 @@ if __name__ == '__main__':
     plt.plot(vn_asympt[min_plot:max_plot],sigma_asympt[0,0,0,min_plot:max_plot].imag,'-o',color='cornflowerblue')
     plt.show()
 
-    print(mf.vn_plus(10,5))
+    # asympt = sigma_dmft.get_asympt(niv_asympt=niv_full,pos=False)
+    # plt.figure()
+    # plt.semilogy(vn_asympt[niv_full:],np.abs(np.mean(sigma_asympt,(0,1,2))[niv_full:].imag-asympt.imag[niv_full:]),'-o',color='cornflowerblue')
+    # plt.semilogy(vn_asympt[niv_full:],sigma_dmft.err*np.ones_like(vn_asympt[niv_full:]),'-',color='k')
+    # plt.ylim(1e-4,1e-3)
+    # plt.xlim(200,300)
+    # plt.show()
+    #
+    # plt.figure()
+    # plt.plot(vn_asympt[niv_full:],np.abs(np.mean(sigma_asympt,(0,1,2))[niv_full:].real-asympt.real[niv_full:]),'-o',color='cornflowerblue')
+    # # plt.plot(vn_asympt[niv_full:],sigma_dmft.err*np.ones_like(vn_asympt[niv_full:]),'-',color='k')
+    # plt.ylim(1e-4,1e-2)
+    # plt.xlim(200,1000)
+    # plt.show()
+    # print(mf.vn_plus(10,5))
+    # print(np.argmax(np.abs(np.mean(sigma_asympt,(0,1,2))[niv_full:].real-asympt.real[niv_full:]) < sigma_dmft.err))
+    # print(np.argmax(np.abs(np.mean(sigma_asympt,(0,1,2))[niv_full:].imag-asympt.imag[niv_full:]) < sigma_dmft.err))
